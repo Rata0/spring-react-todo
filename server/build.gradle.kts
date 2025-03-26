085e383822463ff3e201161b744867fec4a5fd62
Rata0 <nekon689@gmail.com>
2025-03-26 20:17:45 +0300

    add driver postgres and fix properties

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
