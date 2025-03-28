ea3a3149f7a22ca20384670dc3fe7e2d5eb0a3a6
Rata0 <nekon689@gmail.com>
2025-03-28 14:39:34 +0300

    fix code

--- server/src/main/java/com/hexlet/demo/model/Todo.java
+++ server/src/main/java/com/hexlet/demo/model/Todo.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,6 +20,10 @@ public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     private String title;
     private boolean completed;
