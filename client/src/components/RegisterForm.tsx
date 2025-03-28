573bf181abb6ed6d6f11a9e81fabf6d1f2f448f2
Rata0 <nekon689@gmail.com>
2025-03-28 13:27:16 +0300

    added styles to the registration form

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -38,31 +38,36 @@ const RegisterForm = () => {
 
   return (
     <div>
-      <h2>Register</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="username">Username:</label>
+          <label htmlFor="username" className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+            Your Username
+          </label>
           <input
             type="text"
             id="username"
             name="username"
             value={user.username}
             onChange={handleChange}
+            className='shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light'
             required
           />
         </div>
         <div>
-          <label htmlFor="password">Password:</label>
+          <label htmlFor="password" className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+            Your Password
+          </label>
           <input
             type="password"
             id="password"
             name="password"
             value={user.password}
             onChange={handleChange}
+            className='shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light'
             required
           />
         </div>
-        <button type="submit">Register</button>
+        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
       </form>
       {message && (
         <div style={{ color: isError ? 'red' : 'green' }}>
